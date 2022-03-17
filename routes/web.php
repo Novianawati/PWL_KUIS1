a9db95dc5e2aae8cf386295e69ee43268760dca4
novita824 <snovita819@gmail.com>
2022-03-17 04:47:20 -0700

    routes

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,16 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/home', function () {
+    return view('home');
+});
+
+Route::prefix('/collection')->group(function(){
+    Route::get('/', function(){
+
+    });
+});
+
+Route::get('/contact', function () {
+    return view('contact');
 });
