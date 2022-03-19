f18717a414f190b93d999e22ca32ab328147e8b8
Novianawati <novianawati92@gmailcom>
2022-03-20 05:29:04 +0700

    pegawai controller

--- app/Http/Controllers/PegawaiController.php
+++ app/Http/Controllers/PegawaiController.php
@@ -3,8 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\pegawai;
 
 class PegawaiController extends Controller
 {
-    //
+    public function pg(){
+        $data = pegawais::all();
+
+        return view('home');
+    }
 }
